f649e83f8ed24497a343d1dad1de2ea3557bbead
ProjectsUSD <projectsusd@gmail.com>
2026-03-28 14:45:42 -0500

    fix(deploy): remove database migrations from build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,4 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Run database migrations
-echo "Running database migrations..."
-alembic upgrade head
-
 echo "Build completed successfully!"
